1d5e98a0c456087254068858b8e5b091cc74d77f
Apelicious <Apelicious@DESKTOP-7OGS3S6>
2023-06-14 15:03:08 +0700

    Add project files.

---
+++ exepemdas3_20220140046/exepemdas3_20220140046.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class penerbit {
+public:
+	string nama;
+
+	orang(string Gama Press) :
+		nama(pNama) {
+		cout << "pengarang pada penerbit dibuat\n" << endl;
+	}
+
+	~orang() {
+		cout << "Joko, Lia, Giga\n" << endl;
+	}
+
+	class penerbit : public orang {
+		cout << "pengarang pada penerbit dibuat\n" << endl;
+	}
+    
+	class penerbit
+		cout << "pengerang\n" << endl;
+		cout << "penerbit\n" << endl;
+		cout << "buku\n" << endl;
+
+int main() {
+	pengarang penerbit buku
+		cout << "hasil =" <<
+
+		return 0;
+
+}
+
+
+
+
+
+
+};
